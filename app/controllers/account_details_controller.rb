e05264b00519891f59047d0a83b14e38c45f3430
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-04-25 16:36:22 +0100

    Add trello integration to create a trello card

--- app/controllers/account_details_controller.rb
+++ app/controllers/account_details_controller.rb
@@ -5,8 +5,14 @@ class AccountDetailsController < ApplicationController
 
   def post
     form_params = params.fetch('request_an_aws_account_form', {})
-    @form = RequestAnAwsAccountForm.new(form_params)
-    session['form'] = form_params.to_json
+    all_params = session.fetch('form', {}).merge form_params.permit(
+      :account_name,
+      :programme,
+      :is_production,
+      :admin_users
+    )
+    @form = RequestAnAwsAccountForm.new(all_params.with_indifferent_access)
+    session['form'] = all_params
     redirect_to programme_path
   end
 end
--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,3 +1,10 @@
+require 'trello'
+
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
+  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
+
 class AdministratorsController < ApplicationController
   def administrators
     @form = RequestAnAwsAccountForm.new({})
@@ -5,10 +12,22 @@ class AdministratorsController < ApplicationController
 
   def post
     form_params = params.fetch('request_an_aws_account_form', {})
-    @form = RequestAnAwsAccountForm.new(form_params)
+    all_params = session.fetch('form', {}).merge form_params.permit(
+      :account_name,
+      :programme,
+      :is_production,
+      :admin_users
+    )
+    @form = RequestAnAwsAccountForm.new(all_params.with_indifferent_access)
+    session['form'] = all_params
 
     # This is the last controller, so we should do the side effects now.
     # Initially let's just stick a card in trello
+    Trello::Card.create(
+      list_id: '5ade08f50b5895b033065f71',
+      name: "#{@form.account_name} (#{@form.programme})",
+      desc: "New AWS account requested\n\n```\n#{JSON.pretty_generate(all_params)}\n```\n\n"
+    )
 
     redirect_to confirmation_path
   end

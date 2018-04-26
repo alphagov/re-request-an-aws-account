ca563d2f8d351ef96fe8d5d721407a02644c9743
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-04-26 10:29:41 +0100

    Create a PR from the form input

--- app/controllers/administrators_controller.rb
+++ app/controllers/administrators_controller.rb
@@ -1,4 +1,5 @@
 require 'trello'
+require 'octokit'
 
 Trello.configure do |config|
   config.developer_public_key = ENV['TRELLO_DEVELOPER_PUBLIC_KEY']
@@ -23,12 +24,37 @@ class AdministratorsController < ApplicationController
 
     # This is the last controller, so we should do the side effects now.
     # Initially let's just stick a card in trello
+    pr_url = create_pull_request(JSON.pretty_generate(all_params))
     Trello::Card.create(
       list_id: '5ade08f50b5895b033065f71',
       name: "#{@form.account_name} (#{@form.programme})",
-      desc: "New AWS account requested\n\n```\n#{JSON.pretty_generate(all_params)}\n```\n\n"
+      desc: "New AWS account requested\n\nA pull request has been generated for you: #{pr_url}"
     )
 
     redirect_to confirmation_path
+  end
+
+  def create_pull_request(new_value)
+    new_branch_name = 'bau-test-octokit-' + SecureRandom.alphanumeric(6)
+    client = Octokit::Client.new(access_token: ENV['GITHUB_PERSONAL_ACCESS_TOKEN'])
+    master = client.commit('richardTowers/re-example-repo', 'master')
+    client.create_ref 'richardTowers/re-example-repo', 'heads/' + new_branch_name, master.sha
+
+    contents = client.contents 'richardTowers/re-example-repo', path: 'terraform/example/scratch.json'
+    client.update_contents(
+      'richardTowers/re-example-repo',
+      'terraform/example/scratch.json',
+      'Updating content with octokit',
+      contents.sha,
+      new_value,
+      branch: new_branch_name
+    )
+    client.create_pull_request(
+      'richardTowers/re-example-repo',
+      'master',
+      new_branch_name,
+      'Test a PR from octokit',
+      'Pair: @richardTowers'
+    ).html_url
   end
 end

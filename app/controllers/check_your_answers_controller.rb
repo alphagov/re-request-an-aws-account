3e5a15817846b58738ac0e0d262b1ef1acbefa4b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-05-30 11:03:34 +0100

    Add account_description and admin_users to email personalization
    This will allow me to update the Notify template for this email so that
    it includes the description of the account and the list of admin users.

--- app/controllers/check_your_answers_controller.rb
+++ app/controllers/check_your_answers_controller.rb
@@ -23,7 +23,14 @@ class CheckYourAnswersController < ApplicationController
     session['pull_request_url'] = pull_request_url
 
     notify_service = NotifyService.new
-    notify_service.new_account_email_support(account_name: account_name, programme: programme, email: email, pull_request_url: pull_request_url)
+    notify_service.new_account_email_support(
+      account_name: account_name,
+      account_description: account_description,
+      programme: programme,
+      email: email,
+      pull_request_url: pull_request_url,
+      admin_users: admin_users
+    )
     notify_service.new_account_email_user email, account_name, pull_request_url
 
     redirect_to confirmation_account_path

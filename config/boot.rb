2370176a544f55c54151ed51aa5b9990101b414d
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-05-31 12:33:09 +0100

    Disable webrick's logging
    Neither useful nor formatted correctly.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup'
+
+require 'rails/command'
+require 'rails/commands/server/server_command'
+
+module Rails
+  class Server < ::Rack::Server
+    alias_method :orig_initialize, :initialize
+    def initialize(options)
+      orig_initialize(options.merge(log_stdout: false, AccessLog: []))
+    end
+  end
+end

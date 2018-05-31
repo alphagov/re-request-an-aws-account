04803e80cef8978a4fc4697c57ce1eb8c92c64a7
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-05-31 16:30:23 +0100

    Hack: log to STDERR instead of STDOUT
    Some kind of weird interaction between lograge and cloudfoundry means it
    won't work with logging going to STDOUT...
    
    Logging to STDERR instead appears to fix the issue :shrug:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,11 +8,6 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.new
   config.force_ssl = true
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  config.logger = ActiveSupport::Logger.new(STDERR)
 end

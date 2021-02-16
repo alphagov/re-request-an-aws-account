3ade0e7aaf3c1e347ab4ca062219d22356768eef
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2021-02-16 16:26:38 +0000

    Put log_error definition somewhere central
    It's pretty generic and likely to be helpful in many places.

--- app/lib/errors.rb
+++ app/lib/errors.rb
@@ -1,2 +1,12 @@
 class EmailTooLongError < StandardError
 end
+
+def log_error(description, error = nil)
+  Rails.logger.error({
+    '@timestamp': Time.now.iso8601,
+    description: description,
+    message: error ? error.message : nil,
+    backtrace: error ? error.backtrace.join("\n") : nil
+  }.to_json)
+  nil
+end

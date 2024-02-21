07dbe2486cbf180a4ab229250becb4882b488647
David Heath <david.heath@digital.cabinet-office.gov.uk>
2024-02-21 17:57:05 +0000

    ENG-72 other changes from rails app:update
    This are more substantive changes. I've reviewed them and don't see anything which will cause a problem

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,15 @@ module ReRequestAnAwsAccount
     # workaround until https://github.com/sass/libsass/milestone/35 is shipped
     config.assets.css_compressor = nil
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end

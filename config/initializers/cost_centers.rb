ac7b35219fec292c61b190afc30b38d8b97f97e3
Corin Wilkins <1826895+CorinWilkins@users.noreply.github.com>
2024-10-01 12:19:43 +0100

    Add better error handling around cost center csv file getting.
    * Explicit error message if env file is not set
    * doesn't load fake data unless running in development
    *errors out if unable to get csv file from s3

--- config/initializers/cost_centers.rb
+++ config/initializers/cost_centers.rb
@@ -14,11 +14,17 @@ def load_from_s3(bucket, key)
 end
 
 def get_cost_centre_data()
-  begin
-    return load_from_s3(ENV['COST_CENTRE_S3_BUCKET_NAME'], 'cost_centres.csv')
-  rescue Aws::S3::Errors::ServiceError, Aws::Errors::MissingRegionError
-    Rails.logger.error("Failed unable to retrieve cost_centres.csv from s3")
+  if Rails.env.development?
     return File.read(File.join(Rails.root, 'config', 'cost_centre_fixture.csv'))
+  end
+
+  begin
+    bucket = ENV['COST_CENTRE_S3_BUCKET_NAME']
+    raise 'Failed COST_CENTRE_S3_BUCKET_NAME env var is NOT set' unless bucket
+
+    return load_from_s3(bucket, 'cost_centres.csv')
+  rescue Aws::S3::Errors::ServiceError, Aws::Errors::MissingRegionError
+    raise "Failed unable to retrieve cost_centres.csv from s3"
   end
 end
 
